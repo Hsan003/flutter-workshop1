afb08136dcf3eda3f014825f2e47c46b0c990a14
aymenbouhaha <92325496+aymenbouhaha@users.noreply.github.com>
2023-11-07 17:44:23 +0100

    first commit

---
+++ lib/shared/styles/assets.dart
@@ -0,0 +1,31 @@
+
+
+class AppAssets {
+
+  // Icons
+
+  static String bikeIcon = 'assets/icons/bike.png';
+  static String boxIcon = 'assets/icons/box.png';
+  static String creditCardIcon = 'assets/icons/credit-card.png';
+  static String droneIcon = 'assets/icons/Drone.png';
+  static String gridIcon = 'assets/icons/grid.png';
+  static String heartIcon = 'assets/icons/heart.png';
+  static String homeIcon = 'assets/icons/home.png';
+  static String searchIcon = 'assets/icons/search.png';
+  static String shoppingCartIcon = 'assets/icons/shopping-cart.png';
+  static String userIcon = 'assets/icons/user.png';
+  static String walkingIcon = 'assets/icons/walking.png';
+
+  // Images
+
+  static String bostonLettuce = 'assets/images/boston-lettuce.jpeg';
+  static String bread = 'assets/images/bread.jpeg';
+  static String card = 'assets/images/Card.png';
+  static String fruits = 'assets/images/fruits.jpg';
+  static String purpleCauliflower = 'assets/images/purple-cauliflower.jpeg';
+  static String splashScreenBackground = 'assets/images/splash-screen-background.png';
+  static String sweets = 'assets/images/sweets.jpeg';
+  static String vegetables = 'assets/images/vegetable.jpeg';
+
+
+}
---
+++ lib/shared/styles/colors.dart
@@ -0,0 +1,28 @@
+
+
+
+import 'dart:ui';
+
+class AppColors {
+
+
+  // Text Colors
+
+  static Color textPrimary = const Color(0xff2D0C57);
+  static Color textSecondary = const Color(0xff9586A8);
+  static Color selectedViolet = const Color(0xff7203FF);
+
+
+  // Borders and Background
+
+  static Color selectedBackgroundViolet = const Color(0xffE2CBFF);
+  static Color border = const Color(0xffD9D0E3);
+  static Color background = const Color(0xffF6F5F5);
+
+
+  static Color primaryButton = const Color(0xff0BCE83);
+
+
+
+
+}
